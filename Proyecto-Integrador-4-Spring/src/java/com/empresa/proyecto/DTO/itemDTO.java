ea8c0b39fc6c2fd952cdb590682350f066b22af8
Rodrigo Mejia Diaz <rodrigoemdiaz@hotmail.com>
2019-12-07 17:06:17 -0500

    Quitado subtotal

--- Proyecto-Integrador-4-Spring/src/java/com/empresa/proyecto/DTO/itemDTO.java
+++ Proyecto-Integrador-4-Spring/src/java/com/empresa/proyecto/DTO/itemDTO.java
@@ -13,7 +13,6 @@ public class itemDTO {
     
     private productoDTO producto;
     private int cantidad;
-    private int subtotal;
 
     public productoDTO getProducto() {
         return producto;
@@ -30,24 +29,14 @@ public class itemDTO {
     public void setCantidad(int cantidad) {
         this.cantidad = cantidad;
     }
-
-    public int getSubtotal() {
-        return subtotal;
-    }
-
-    public void setSubtotal(int subtotal) {
-        this.subtotal = subtotal;
-    }
-    
     
     public itemDTO(){
         
     }
 
-    public itemDTO(productoDTO producto, int cantidad, int subtotal) {
+    public itemDTO(productoDTO producto, int cantidad) {
         this.producto = producto;
         this.cantidad = cantidad;
-        this.subtotal = subtotal;
     }
     
     
